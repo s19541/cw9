294946c4fa8750b9082c76b0a803ed8002a9c6e7
Marcin Wałachowski <s19541@pjwstk.edu.pl>
2020-05-05 12:12:11 +0200

    cw9

---
+++ LinqConsoleApp/LinqConsoleApp/LinqConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LinqConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new LinqSamples();
+            d.LoadData();
+            d.Przyklad3();
+        }
+    }
+}
